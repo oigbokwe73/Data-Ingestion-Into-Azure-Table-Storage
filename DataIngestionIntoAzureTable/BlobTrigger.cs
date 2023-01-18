911526dabc1c31f1e9f934c77a4c75f3f6724126
obinna <oigbokwe73@gmail.com>
2023-01-18 16:15:00 -0500

    updates

---
+++ DataIngestionIntoAzureTable/BlobTrigger.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Specialized;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Xenhey.BPM.Core.Net6;
+using Xenhey.BPM.Core.Net6.Implementation;
+
+namespace DataIngestionIntoAzureTable
+{
+    public class BlobTrigger
+    {
+        [FunctionName("BlobTrigger")]
+        public static void Run([BlobTrigger("processed/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob, string name, ILogger log)
+        {
+
+            string ApiKeyName = "x-api-key";
+            log.LogInformation("C# blob trigger function processed a request.");
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add(ApiKeyName, "43EFE991E8614CFB9EDECF1B0FDED37A");
+            IOrchestrationService orchrestatorService = new ManagedOrchestratorService(nvc);
+            var processFiles = orchrestatorService.Run(myBlob);
+            log.LogInformation($"Following has been completed {name}");
+        }
+    }
+}
---
+++ DataIngestionIntoAzureTable/create.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+using System.Linq;
+using Xenhey.BPM.Core.Net6;
+using Xenhey.BPM.Core.Net6.Implementation;
+
+namespace DataIngestionIntoAzureTable
+{
+    public class Create
+    {
+        private HttpRequest _req;
+        private NameValueCollection nvc = new NameValueCollection();
+        [FunctionName("Create")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequest req, ILogger log)
+        {
+            _req = req;
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = await new StreamReader(_req.Body).ReadToEndAsync();
+            _req.Headers.ToList().ForEach(item => { nvc.Add(item.Key, item.Value.FirstOrDefault()); });
+            var results = orchrestatorService.Run(requestBody);
+            return resultSet(results);
+
+        }
+
+        private ActionResult resultSet(string reponsePayload)
+        {
+            var returnContent = new ContentResult();
+            var mediaSelectedtype = nvc.Get("Content-Type");
+            returnContent.Content = reponsePayload;
+            returnContent.ContentType = mediaSelectedtype;
+            return returnContent;
+        }
+        private IOrchestrationService orchrestatorService
+        {
+            get
+            {
+                return new ManagedOrchestratorService(nvc);
+            }
+        }
+
+    }
+}
